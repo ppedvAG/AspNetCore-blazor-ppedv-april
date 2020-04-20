06acce2827f4fb9337540c76dad3eadc7a35c460
Kevin Winter <KevinW@ppedv.de>
2020-04-20 12:41:57 +0200

    Grundaufbau von RazorPages und MVC
    Razor Engine Erklärung

---
+++ AspNetCore_2020_04_20/AspNetCore_Razor_WithMVC/Controllers/RazorController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore_Razor_WithMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore_Razor_WithMVC.Controllers
+{
+    public class RazorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ShowList ()
+        {
+            ToDo aufgabe1 = new ToDo();
+            aufgabe1.Title = "Gassi gehen";
+            aufgabe1.Beschreibung = "Bitte gehe eine Stunde mit dem Hund raus";
+            aufgabe1.BisWann = DateTime.Now;
+            aufgabe1.IstAktiv = false;
+
+            ToDo aufgabe2 = new ToDo();
+            aufgabe2.Title = "Essen kochen";
+            aufgabe2.Beschreibung = "Bitte nur vegetarisch";
+            aufgabe2.BisWann = DateTime.Now;
+            aufgabe2.IstAktiv = true;
+
+            ToDo aufgabe3 = new ToDo();
+            aufgabe3.Title = "Programmieren";
+            aufgabe3.Beschreibung = "Bug behebeb";
+            aufgabe3.BisWann = DateTime.Now;
+            aufgabe3.IstAktiv = true;
+
+            IList<ToDo> aufgabenListe = new List<ToDo>();
+            aufgabenListe.Add(aufgabe1);
+            aufgabenListe.Add(aufgabe2);
+            aufgabenListe.Add(aufgabe3);
+
+
+            return View(aufgabenListe);
+        }
+
+        public IActionResult ShowToDoWithRazor()
+        {
+            ToDo aufgabe1 = new ToDo();
+            aufgabe1.Title = "Gassi gehen";
+            aufgabe1.Beschreibung = "Bitte gehe eine Stunde mit dem Hund raus";
+            aufgabe1.BisWann = DateTime.Now;
+            aufgabe1.IstAktiv = false;
+
+            return View(aufgabe1);
+        }
+    }
+}
---
+++ AspNetCore_2020_04_20/AspNetCore_Razor_WithMVC/Models/ToDo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCore_Razor_WithMVC.Models
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+
+        public string Beschreibung { get; set; }
+
+        public DateTime BisWann { get; set; }
+
+        public bool IstAktiv { get; set; }
+    }
+}
